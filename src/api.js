9dfbcc3f29bc11aa0818feacc1d8c04c8aa5ec0c
Mark R. Hughes <mrhughes@xogrp.com>
2022-02-11 12:14:17 -0600

    minor change committed to form

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// mrhughes local changes
+
 export default {
     getProduct: function(){
         return axios.get("https://qa-registry-interview-api.regsvcs.theknot.com/products");
